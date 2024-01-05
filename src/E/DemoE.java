87fa897280520ace5b1e493935b60e9b78374dac
SolianaYemane <155404423+SolianaYemane@users.noreply.github.com>
2024-01-05 11:59:36 -0800

    Update DemoE.java

--- src/E/DemoE.java
+++ src/E/DemoE.java
@@ -4,8 +4,10 @@ public class DemoE {
 	public static void main(String[] args) {
 		System.out.println("Friday");
 		System.out.println("Sprint 5 Done");  
-
 		System.out.println("Christmas"); 
 	}
+	public static void add(int a, int b, int c){
+	System.out.println(a+b+c);  
+	} 
 
 }  
